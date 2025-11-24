40b579e547008f7664d1df3d2a1405fa1c047489
Deep Saha <dip23447@gmail.com>
2025-11-24 17:14:55 +0530

    Modified Q_30

--- P_05/RecursiveDeletion_30.py
+++ P_05/RecursiveDeletion_30.py
@@ -1,9 +1,14 @@
+def empty(s, p):
+    if s == "": return True
+    if p not in s: return False
+    i = s.find(p)
+    while i != -1:
+        if empty(s[:i] + s[i+len(p):], p):
+            return True
+        i = s.find(p, i+1)
+    return False
+
 s = input("Enter string: ")
+p = input("Enter substring: ")
 
-while len(s) > 1 and s[0] == s[-1]:
-    s = s[1:-1]
-
-if s == "":
-    print("Can become empty")
-else:
-    print("Cannot become empty")
+print("Can become empty" if empty(s, p) else "Cannot become empty")

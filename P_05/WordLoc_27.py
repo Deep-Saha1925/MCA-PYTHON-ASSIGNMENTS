e4d9c51c61ff1044ffd16b1c4a9a1d0baa471939
Deep Saha <dip23447@gmail.com>
2025-11-24 17:04:42 +0530

    Modified Q_27

--- P_05/WordLoc_27.py
+++ P_05/WordLoc_27.py
@@ -1,3 +1,9 @@
-s = input("Enter sentence: ").split()
-word = input("Enter word: ")
-print(s.index(word))
+s = input("Enter sentence: ").lower()
+word = input("Enter word: ").lower()
+
+index = s.find(word)
+
+if index != -1:
+    print(f"Word found at index: {index}")
+else:
+    print("Word not found in the string.")

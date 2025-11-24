e8676d576cb72e27c7ee798d37d7b2838e03fc9b
Deep Saha <dip23447@gmail.com>
2025-11-24 17:00:48 +0530

    Modified Q_25

--- P_05/StringPermutations_25.py
+++ P_05/StringPermutations_25.py
@@ -1,9 +1,20 @@
-def permute(s, ans=""):
-    if s == "":
-        print(ans)
-        return
-    for i in range(len(s)):
-        permute(s[:i] + s[i+1:], ans + s[i])
+# def permute(s, ans=""):
+#     if s == "":
+#         print(ans)
+#         return
+#     for i in range(len(s)):
+#         permute(s[:i] + s[i+1:], ans + s[i])
 
-s = input("Enter string: ")
-permute(s)
+# s = input("Enter string: ")
+# permute(s)
+
+from itertools import permutations
+
+s = input("Enter a string: ")
+
+# generate permutations
+perms = permutations(s)
+
+print("Permutations:")
+for p in perms:
+    print("".join(p))

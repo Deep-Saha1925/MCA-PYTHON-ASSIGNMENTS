9d1679adcbf565e4891b56b6bdd2cfe1b98870f7
Deep Saha <dip23447@gmail.com>
2026-01-14 22:37:02 +0530

    Finished 2 Programs of Function

---
+++ Function/AddNumbers_2.py
@@ -0,0 +1,10 @@
+def add_numbers(a, b):
+    return a + b
+
+# Taking input from user
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+# Function call
+result = add_numbers(num1, num2)
+print("Sum =", result)

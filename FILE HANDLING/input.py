428a20f1e0a57ada4a905ac3d6f5c24af8ffb88e
Deep Saha <dip23447@gmail.com>
2026-01-27 11:49:07 +0530

    Updated dict code

--- FILE HANDLING/input.py
+++ FILE HANDLING/input.py
@@ -6,10 +6,10 @@ with open("input.txt", "r") as file:
         c += 1
         l.append(int(f))
         s += int(f)
-        
-d = {
-    "count": c,
-    "Sum": s
-}
+       
+d = dict()
+d["count"] = c
+d["Sum"] = s
+
 with open("output.txt", "w") as file:
     file.write(f'{c} input items.\nSum = {s}\n{l}\n{d}')

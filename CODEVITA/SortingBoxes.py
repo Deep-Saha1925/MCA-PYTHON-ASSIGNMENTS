2f9b13debc8ecaf7367abd4165a382b3d2eac564
Deep Saha <dip23447@gmail.com>
2026-01-29 14:42:48 +0530

    Removed unwanted test cases.

--- CODEVITA/SortingBoxes.py
+++ CODEVITA/SortingBoxes.py
@@ -39,8 +39,4 @@ def minimum_effort(n, k, boxes):
 n, k = map(int, input("Enter N and K: ").split())
 boxes = list(map(int, input("Enter weights: ").split()))
 
-# n = 5
-# k = 2
-# boxes = [20, 50, 30, 80, 70]
-
 print(minimum_effort(n, k, boxes))

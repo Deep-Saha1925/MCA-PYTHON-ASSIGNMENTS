5f04ffaadfae4d3cdeb575469ccc4d1ea029de2c
Deep Saha <dip23447@gmail.com>
2026-02-02 17:07:16 +0530

    Solved in another way

--- CODEVITA/PhilalandCoin.py
+++ CODEVITA/PhilalandCoin.py
@@ -1,6 +1,25 @@
-import math
+# Using LOG
+# import math
 
-T = int(input().strip())
+# T = int(input().strip())
+# for _ in range(T):
+#     N = int(input().strip())
+#     print(math.ceil(math.log2(N + 1)))
+
+
+
+def min_denominations(N):
+    coverage = 0
+    count = 0
+
+    while coverage < N:
+        coin = coverage + 1
+        coverage += coin
+        count += 1
+
+    return count
+
+T = int(input())
 for _ in range(T):
-    N = int(input().strip())
-    print(math.ceil(math.log2(N + 1)))
+    N = int(input())
+    print(min_denominations(N))
